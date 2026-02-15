34261b0a6fe4dd83e07124dc72d4f8a43b37f1bd
Hasan Safari <hasan5afari@yahoo.com>
2026-02-15 21:54:53 +0330

    add built-in echo command

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 import sys
 
 
-def main() -> None:
+def process_input(input: str) -> tuple[str, list[str]]:
+    tokens = input.split(" ")
+    num_tokens = len(tokens)
+
+    command = tokens[0] if num_tokens > 0 else ""
+    extra_stuff = tokens[1:] if num_tokens > 1 else []
+
+    return command, extra_stuff
+
+
+def main():
     while True:
         sys.stdout.write("$ ")
 
-        user_command = input()
+        command, extra = process_input(input())
 
-        if user_command == "exit":
+        if command == "exit":
             exit()
-
-        print(f"{user_command}: command not found")
+        elif command == "echo":
+            if extra:
+                print(" ".join(extra))
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
